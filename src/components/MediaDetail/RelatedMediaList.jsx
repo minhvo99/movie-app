e0fccbb7fc014f636e1026681326b77004c0d01a
minhvo99 <pikachu43dn@gmail.com>
2025-05-17 23:17:50 +0700

    style related media component

--- src/components/MediaDetail/RelatedMediaList.jsx
+++ src/components/MediaDetail/RelatedMediaList.jsx
@@ -9,7 +9,7 @@ const RelatedMediaList = ({ mediaList, isLoading, title }) => {
       {isLoading ? (
         <Loading />
       ) : (
-        <div className="grid grid-cols-3 gap-4 sm:grid-cols-4 lg:grid-cols-4 lg:gap-6">
+        <div className="grid grid-cols-2 gap-4 sm:grid-cols-4 lg:grid-cols-4 lg:gap-6">
           {(mediaList || []).map((media) => (
             <MovieCard
               key={media.id}
